10a0fd88a11eb3c6a3c076699c2f461ab9b9437d
mirai-mirai <shiozaki.takashi@gmail.com>
2023-08-02 04:57:52 +0900

    docsにビルドするよう修正

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  // github pagesの変な仕様（ルートが１つ上）に対応する
+  base: '/20230802_SimpleCameraApp/',
   plugins: [vue()],
+  build: {
+    outDir: 'docs', // ビルド出力先を変更
+    rollupOptions: {
+      output: {
+        assetFileNames: '[name]-[hash][extname]',
+        chunkFileNames: '[name]-[hash].js', // JSをルートに出力
+        entryFileNames: '[name]-[hash].js', // JSをルートに出力
+      },
+    },
+  },
 })
+
