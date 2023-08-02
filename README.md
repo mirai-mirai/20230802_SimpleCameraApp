237d8445ae288a6555ba26ad826c6d8773d555d6
mirai-mirai <shiozaki.takashi@gmail.com>
2023-08-03 01:10:05 +0900

    デバイスリスト追加

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 
 Very simple camera app for the test porpose. It has only Start & Stop Buttons.
 
+demo:https://mirai-mirai.github.io/20230802_SimpleCameraApp/
+
--- src/components/Camera.vue
+++ src/components/Camera.vue
@@ -4,25 +4,43 @@ import { ref } from 'vue'
 const $video = ref<HTMLVideoElement>()
 const $startBtn = ref<HTMLButtonElement>()
 const $stopBtn = ref<HTMLButtonElement>()
+const $devices = ref<MediaDeviceInfo[]>([])
+const selectedDevId = ref<string | null>(null)
 let stream: MediaStream
 
 const startVideo = async () => {
-  const cfg = { video: true, audio: false }
+  const deviceId = selectedDevId.value as ConstrainDOMString
+  const cfg: MediaStreamConstraints = { video: { deviceId }, audio: false }
   stream = await navigator.mediaDevices.getUserMedia(cfg)
   $video.value!.srcObject = stream
   $video.value!.play()
 }
 
 const stopVideo = () => {
+  if (!stream) return
   stream.getTracks().forEach(track => track.stop())
+  
   $video.value!.pause()
   $video.value!.srcObject = null
+}
+
+const getDevices = async () => {
+  const devices = (await navigator.mediaDevices.enumerateDevices())
+  $devices.value = devices.filter(({ kind }) => kind === 'videoinput');
+  selectedDevId.value = $devices.value[0].deviceId
+}
+
+const changeDevice = () => {
+  const nowPlaying = !$video.value!.paused
+  stopVideo()
+  if (nowPlaying) startVideo()
 }
 
 window.onload = async () => {
   console.log('window.onload')
   $startBtn.value!.addEventListener('click', startVideo)
   $stopBtn.value!.addEventListener('click', stopVideo)
+  getDevices()
 }
 
 </script>
@@ -30,7 +48,11 @@ window.onload = async () => {
 <template>
   <h1>Simple Camera</h1>
   <h5>-- T.shiozaki 2023/8/2 --</h5>
-
+  Select Camera:
+  <div v-for="{ deviceId, label } in $devices" :key="deviceId">
+    <input type="radio" v-model="selectedDevId" :value="deviceId" @change="changeDevice">
+    <label>{{ label }}</label>
+  </div>
   <button ref="$startBtn">start</button>
   <button ref="$stopBtn">stop</button> <br>
   <video ref="$video" autoplay></video>
@@ -38,11 +60,11 @@ window.onload = async () => {
 
 <style scoped>
 h1 {
-  margin-bottom: 0;
+  margin: 0px 0px 0px 0px;
 }
 
 h5 {
-  margin-top: 0;
+  margin: 0px 0px 10px 0px;
 }
 
 video {
